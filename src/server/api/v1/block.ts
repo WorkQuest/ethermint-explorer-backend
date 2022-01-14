d5efee0c11df15cb980118dde995f62b65c912ea
Ilya Kasimov <adamoffilya@mail.ru>
2022-01-14 12:25:49 +0700

    [update] add transactions in function getBlockById

--- src/server/api/v1/block.ts
+++ src/server/api/v1/block.ts
@@ -1,6 +1,7 @@
 import { Block } from '../../models/Block';
 import { error, output } from '../../utils';
 import { Errors } from '../../utils/errors';
+import { Tx } from '../../models/Tx';
 
 export async function getBlocks(r) {
   const {count, rows} = await Block.findAndCountAll({
@@ -13,13 +14,15 @@ export async function getBlocks(r) {
 }
 
 export async function getBlockById(r) {
-  const block = await Block.findByPk(r.params.blockId);
+  const block = await Block.findByPk(r.params.blockId, {
+    include: [{
+      model: Tx,
+      as: 'txs'
+    }]
+  });
+
   if (!block)
     return error(Errors.NotFound, 'Block not found', {})
 
   return output(block);
 }
-
-// export async function getBlockTxs(r) {
-//   let
-// }
